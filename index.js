88ad5b4cd4524a76233ba728ed28c78e5f049b1d
anushka.jain <anushka.jain@innovasolutions.com>
2024-12-30 17:40:41 +0530

    add lodash as it provides some lib and func to apply on arr

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-console.log("hello world");
-process.exitCode = 0
+// console.log("hello world");
+// process.exitCode = 0
+
+const _ = require("lodash")
+const arr = [1,2,34,5];
+console.log(_.chunk(arr));
