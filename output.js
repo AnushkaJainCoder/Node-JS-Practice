6b211162d9fe22a67c60fe98662f4cc5b5de209e
anushka.jain <anushka.jain@innovasolutions.com>
2024-12-30 16:01:21 +0530

    learning console.count

--- output.js
+++ output.js
@@ -3,3 +3,11 @@ const y = 'Anu';
 console.log(x,y);
 console.log("I am %s and my age is %d", "Anu", 21);
 
+console.count("A");
+console.count("A");
+console.count("A");
+console.count("D");
+console.countReset("A");
+console.count("A")
+
+
