6e7454531d065618911d736615e911b8d930bb52
anushka.jain <anushka.jain@innovasolutions.com>
2024-12-30 16:17:45 +0530

    track the timing using console.time

--- output.js
+++ output.js
@@ -1,13 +1,40 @@
-const x = '1';
-const y = 'Anu';
-console.log(x,y);
-console.log("I am %s and my age is %d", "Anu", 21);
+// const x = '1';
+// const y = 'Anu';
+// console.log(x,y);
+// console.log("I am %s and my age is %d", "Anu", 21);
 
-console.count("A");
-console.count("A");
-console.count("A");
-console.count("D");
-console.countReset("A");
-console.count("A")
+// console.count("A");
+// console.count("A");
+// console.count("A");
+// console.count("D");
+// console.countReset("A");
+// console.count("A")
+
+
+
+// const func = () => console.trace();
+// const func2 = () => func();
+// func2();
+
+const sum = () => console.log(`the sum of 2 numbers is ${2+1}`);
+const mult = () => console.log(`the multiply of 2 numbers is ${2*3}`);
+
+const measureTime = () => {
+    console.time("sum()");
+    sum();
+    console.timeEnd("sum()");
+    console.time("mult()");
+    mult();
+    console.timeEnd("mult()");
+    
+}
+measureTime()
+
+
+
+
+
+
+
 
 
