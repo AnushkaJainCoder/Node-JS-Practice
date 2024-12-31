e42b183462ddb43f575b20b8e7f40c211904951a
anushka.jain <anushka.jain@innovasolutions.com>
2024-12-31 11:48:39 +0530

    exporting data from 1 file and importing to other

---
+++ car.js
@@ -0,0 +1,15 @@
+// const _ = require("lodash");
+
+const ford = {
+    branch: "FORD",
+    model: "FIESTA",
+}
+
+const tesla = {
+    branch: "TESLA",
+    model: "MODEL 1",
+}
+
+
+// module.exports = ford;
+exports.data = {ford, tesla};
--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-// console.log("hello world");
-// process.exitCode = 0
+// // console.log("hello world");
+// // process.exitCode = 0
 
-const _ = require("lodash")
-const arr = [1,2,34,5];
-console.log(_.chunk(arr));
+// const _ = require("lodash")
+// const arr = [1,2,34,5];
+// console.log(_.chunk(arr));
+
+// const car = require("./car");
+const {data} = require("./car");
+console.log(data);
+
+
