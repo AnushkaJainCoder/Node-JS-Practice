76099b92fa808eb43736cec8c8014aeaf734d5d6
anushka.jain <anushka.jain@innovasolutions.com>
2024-12-31 15:30:33 +0530

    hhandling error using try catch block

--- error.js
+++ error.js
@@ -1,8 +1,17 @@
 // const err = new Error("Error arrived");
 // console.log(err.stack);
 
-const {CustomError} = require('./CustomEror');
-throw new CustomError('Error arrived');
+// const {CustomError} = require('./CustomEror');
+// throw new CustomError('Error arrived');
+
+try{
+    dosomething();
+}
+catch(e){
+    console.log(e);
+    console.log("error arrived");
+    
+}
 
 
 
